2278773c4736e45b2757c282019b343fd55fceab
Oleksandr Dudynets <dudynetss@gmail.com>
2022-04-22 13:00:56 +0300

    Add Ukraine support banner & fix bugs

--- js/modal.js
+++ js/modal.js
@@ -2,7 +2,10 @@ const debateReviewsToolCard = document.getElementById(
   "debate-reviews-tool-card"
 );
 const overflow = document.getElementById("modal-overlay");
+const closeButton = document.getElementById("modal-close");
 const video = document.getElementById("debate-reviews-tool-video");
+const debateReviewsToolModalDescriptionYear = document.getElementById('debateReviewsToolModalDescriptionYear');
+debateReviewsToolModalDescriptionYear.innerText = new Date().getFullYear().toString();
 
 debateReviewsToolCard.addEventListener("click", () => {
   overflow.style.display = "flex";
@@ -10,7 +13,8 @@ debateReviewsToolCard.addEventListener("click", () => {
   video.play();
 });
 
-overflow.addEventListener("click", () => {
+closeButton.addEventListener("click", () => {
   video.pause();
   overflow.style.display = "none";
 });
+
